368bf93f80800ca6511d075ae6099027ab14cf6f
Тимофей <tl79266837738@gmail.com>
2025-10-11 16:17:39 +0300

    Релизация базовых функций

--- pkg/autograd/autograd.go
+++ pkg/autograd/autograd.go
@@ -1,6 +1,8 @@
 package autograd
 
 import (
+	"math"
+
 	"github.com/Hirogava/Go-NN-Learn/pkg/tensor/graph"
 	"github.com/Hirogava/Go-NN-Learn/pkg/tensor/tensor"
 )
@@ -69,4 +71,145 @@ func (e *Engine) ZeroGrad() {
 	for _, node := range e.Nodes {
 		node.ZeroGrad()
 	}
+}
+
+type ReLUOp struct {
+	input *graph.Node
+}
+
+func NewReLUOp(input *graph.Node) *ReLUOp {
+	return &ReLUOp{input: input}
+}
+
+func (e *Engine) ReLU(input *graph.Node) *graph.Node {
+	op := NewReLUOp(input)
+	result := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		if op.input.Value.Data[i] > 0 {
+			result.Data[i] = op.input.Value.Data[i]
+		}
+	}
+	node := graph.NewNode(result, []*graph.Node{input}, op)
+	e.Nodes = append(e.Nodes, node)
+	return node
+}
+
+func (op *ReLUOp) Backward(grad *tensor.Tensor) {
+	gradInput := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		if op.input.Value.Data[i] > 0 {
+			gradInput.Data[i] = grad.Data[i]
+		}
+	}
+	if op.input.Grad == nil {
+		op.input.Grad = tensor.Zeros(op.input.Value.Shape...)
+	}
+	op.input.Grad = gradInput
+}
+
+type SigmoidOp struct {
+	input  *graph.Node
+	output *tensor.Tensor
+}
+
+func NewSigmoidOp(input *graph.Node) *SigmoidOp {
+	return &SigmoidOp{input: input}
+}
+
+func (e *Engine) Sigmoid(input *graph.Node) *graph.Node {
+	op := NewSigmoidOp(input)
+	result := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		result.Data[i] = 1.0 / (1.0 + math.Exp(-op.input.Value.Data[i]))
+	}
+	node := graph.NewNode(result, []*graph.Node{input}, op)
+	e.Nodes = append(e.Nodes, node)
+	return node
+}
+
+func (op *SigmoidOp) Backward(grad *tensor.Tensor) {
+	gradInput := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		s := op.output.Data[i]
+		gradInput.Data[i] = grad.Data[i] * s * (1.0 - s)
+	}
+	if op.input.Grad == nil {
+		op.input.Grad = tensor.Zeros(op.input.Value.Shape...)
+	}
+	op.input.Grad = gradInput
+}
+
+type TanhOp struct {
+	input  *graph.Node
+	output *tensor.Tensor
+}
+
+func NewTanhOp(input *graph.Node) *TanhOp {
+	return &TanhOp{input: input}
+}
+
+func (e *Engine) Tahn(input *graph.Node) *graph.Node {
+	op := NewTanhOp(input)
+	result := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		result.Data[i] = math.Tanh(op.input.Value.Data[i])
+	}
+	node := graph.NewNode(result, []*graph.Node{input}, op)
+	e.Nodes = append(e.Nodes, node)
+	return node
+}
+
+func (op *TanhOp) Backward(grad *tensor.Tensor) {
+	gradInput := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		t := op.output.Data[i]
+		gradInput.Data[i] = grad.Data[i] * (1.0 - t*t)
+	}
+	if op.input.Grad == nil {
+		op.input.Grad = tensor.Zeros(op.input.Value.Shape...)
+	}
+	op.input.Grad = gradInput
+}
+
+type SoftmaxCrossEntropyOp struct {
+	input   *graph.Node
+	target  *tensor.Tensor
+	output  *tensor.Tensor
+	softmax *tensor.Tensor
+}
+
+func NewSoftmaxCrossEntropyOp(input *graph.Node, target *tensor.Tensor) *SoftmaxCrossEntropyOp {
+	return &SoftmaxCrossEntropyOp{input: input, target: target}
+}
+
+func (e *Engine) SoftmaxCrossEntropy(input *graph.Node, target *tensor.Tensor) *graph.Node {
+	op := NewSoftmaxCrossEntropyOp(input, target)
+	maxVal := tensor.Max(op.input.Value)
+	shifted := tensor.Sub(op.input.Value, maxVal)
+
+	exp := tensor.Exp(shifted)
+	sumExp := tensor.Sum(exp)
+	softmax := tensor.Div(exp, sumExp)
+	op.softmax = softmax
+
+	loss := tensor.Zeros(1)
+	for i := range op.input.Value.Data {
+		if op.target.Data[i] > 0 {
+			loss.Data[i] -= math.Log(math.Max(softmax.Data[i], 1e-15)) * op.target.Data[i]
+		}
+	}
+	node := graph.NewNode(loss, []*graph.Node{input}, op)
+	e.Nodes = append(e.Nodes, node)
+	return node
+}
+
+func (op *SoftmaxCrossEntropyOp) Backward(grad *tensor.Tensor) {
+	gradInput := tensor.Zeros(op.input.Value.Shape...)
+	for i := range op.input.Value.Data {
+		gradInput.Data[i] = (op.softmax.Data[i] - op.target.Data[i]) * grad.Data[0]
+	}
+	if op.input.Grad == nil {
+		op.input.Grad = tensor.Zeros(op.input.Value.Shape...)
+	}
+	op.input.Grad = gradInput
 }
